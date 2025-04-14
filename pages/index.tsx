5c07c4d4050b361460619adfa98a5aae88227dba
audiencemachine <98191931+audiencemachine@users.noreply.github.com>
2025-04-14 17:35:16 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import URLInput from '../components/URLInput';
 
+const HomePage = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>AI Displacement Scoring Tool</h1>
+      <URLInput />
+    </div>
+  );
+};
+
+export default HomePage;
