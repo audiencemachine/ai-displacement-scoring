6c09d704d6a8d927d3065af5f305982f58a1cc69
audiencemachine <98191931+audiencemachine@users.noreply.github.com>
2025-04-14 17:23:42 -0400

    Create URLInputs.tsx

---
+++ components/URLInputs.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const URLInput = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Enter URL" />
+      <button>Submit</button>
+    </div>
+  );
+};
+
+export default URLInput;
